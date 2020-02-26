b9eaa5c790e5b5b5a32c8e5b37072b3069f56c89
Joe Hayden <eljoeyy@gmail.com>
2020-02-26 13:11:05 -0600

    api calls added

---
+++ src/services/api.js
@@ -0,0 +1,169 @@
+import axios from 'axios'
+
+const baseUrl = 'https://lit-bayou-93209.herokuapp.com/api'
+
+let token = null
+let header = null
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+  setAuthHeader(token)
+}
+
+const setAuthHeader = info => {
+  header = {
+    headers: {
+      Authorization: info
+    }
+  }
+}
+
+// Project
+
+const getProjects = async () => {
+  const projects = await axios.get(`${baseUrl}/projects`, header)
+
+  return projects.data
+}
+
+const getProjectId = async id => {
+  const project = await axios.get(`${baseUrl}/project/${id}`, header)
+
+  return project.data
+}
+
+const createProject = async project => {
+  const newProject = await axios.post(
+    `${baseUrl}/project/create`,
+    project,
+    header
+  )
+
+  return newProject.data
+}
+
+const editProject = async (id, project) => {
+  const updatedProject = await axios.post(
+    `${baseUrl}/project/${id}/update`,
+    project,
+    header
+  )
+
+  return updatedProject.data
+}
+
+const deleteProject = async id => {
+  const deletedProject = await axios.post(
+    `${baseUrl}/project/${id}/delete`,
+    header
+  )
+
+  return deletedProject.data
+}
+
+// Feature OR Bug
+
+const getType = async (projId, type) => {
+  const typeInfo = await axios.get(
+    `${baseUrl}/project/${projId}/${type}s`,
+    header
+  )
+
+  return typeInfo.data
+}
+
+const getTypeById = async (projId, type, featureId) => {
+  const typeInfo = await axios.get(
+    `${baseUrl}/project/${projId}/${type}/${featureId}`,
+    header
+  )
+
+  return typeInfo.data
+}
+
+const createType = async (projId, type, feature) => {
+  const createdType = await axios.post(
+    `${baseUrl}/project/${projId}/${type}/create`,
+    feature,
+    header
+  )
+
+  return createdType.data
+}
+
+const updateType = async (projId, type, featureId, feature) => {
+  const updatedType = await axios.post(
+    `${baseUrl}/project/${projId}/${type}/${featureId}/update`,
+    feature,
+    header
+  )
+
+  return updatedType.data
+}
+
+const deleteType = async (projId, type, featureId) => {
+  const deletedFeature = await axios.post(
+    `${baseUrl}/project/${projId}/${type}/${featureId}/delete`,
+    header
+  )
+
+  return deletedFeature.data
+}
+
+// Comments
+
+const getComments = async (projId, type, typeId) => {
+  const comments = await axios.get(
+    `${baseUrl}/project/${projId}/${type}/${typeId}/comments`,
+    header
+  )
+
+  return comments.data
+}
+
+const createComment = async (projId, type, typeId, comment) => {
+  const createdComment = await axios.post(
+    `${baseUrl}/project/${projId}/${type}/${typeId}/comment/create`,
+    comment,
+    header
+  )
+
+  return createdComment.data
+}
+
+const updateComment = async (projId, type, typeId, comment) => {
+  const updatedComment = await axios.post(
+    `${baseUrl}/project/${projId}/${type}/${typeId}/comment/update`,
+    comment,
+    header
+  )
+
+  return updatedComment.data
+}
+
+const deleteComment = async (projId, type, typeId, commentId) => {
+  const deletedComment = await axios.post(
+    `${baseUrl}/project/${projId}/${type}/${typeId}/comment/${commentId}/delete`,
+    header
+  )
+
+  return deletedComment.data
+}
+
+export default {
+  setToken,
+  getProjects,
+  getProjectId,
+  createProject,
+  editProject,
+  deleteProject,
+  getType,
+  getTypeById,
+  createType,
+  updateType,
+  deleteType,
+  getComments,
+  createComment,
+  updateComment,
+  deleteComment
+}
