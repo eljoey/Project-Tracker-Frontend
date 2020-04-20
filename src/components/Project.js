ca8b80dfc60d467b7ce40ac791f038f74eb86cea
Joe Hayden <eljoeyy@gmail.com>
2020-04-20 09:36:39 -0500

    add admin to project display

--- src/components/Project.js
+++ src/components/Project.js
@@ -19,7 +19,7 @@ const Project = () => {
 
   const renderBugs = () => {
     if (project.bugs) {
-      return project.bugs.map(bug => (
+      return project.bugs.map((bug) => (
         <Link to={`/project/${id}/bug/${bug._id}`} key={bug._id}>
           {bug.name}
         </Link>
@@ -29,7 +29,7 @@ const Project = () => {
 
   const renderFeatures = () => {
     if (project.features) {
-      return project.features.map(feature => (
+      return project.features.map((feature) => (
         <Link to={`/project/${id}/feature/${feature._id}`} key={feature._id}>
           {feature.name}
         </Link>
@@ -37,9 +37,16 @@ const Project = () => {
     }
   }
 
+  const getAdmin = () => {
+    if (project.admin !== undefined) {
+      return project.admin.username
+    }
+  }
+
   return (
     <>
       <div>Title: {project.name}</div>
+      <div>Admin: {getAdmin()}</div>
       <h3>Features</h3>
       <Link to={`/project/${id}/feature/create`}> Create new Feature </Link>
       <div>{renderFeatures()}</div>
