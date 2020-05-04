f251e57f221bf6c5e7a9c995f1e73bebad026393
Joe Hayden <eljoeyy@gmail.com>
2020-05-04 10:03:23 -0500

    shitty projects display

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,23 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { MDBBtn } from 'mdbreact'
+import { MDBBtn, MDBLink } from 'mdbreact'
 
 const Projects = ({ projects }) => {
   const displayProjects = () => {
     return (
-      <>
+      <div>
         <MDBBtn tag={Link} to={'/projects/create'} color="primary" size="sm">
           Create New Project
         </MDBBtn>
-        <div>
+        <div className="text-center">
           {projects.map((proj) => (
-            <div key={proj._id}>
+            <div className=" border border-dark m-1 p-2" key={proj._id}>
               <Link to={`/project/${proj._id}`}>{proj.name}</Link>
               <p>{proj.description}</p>
+              <div className="d-flex justify-content-center">
+                <MDBLink
+                  to={`/project/${proj._id}`}
+                  className="border border-dark rounded pill h-5 w-25 mr-5"
+                >
+                  Features
+                  <span> ({proj.features.length})</span>
+                </MDBLink>
+                <MDBLink
+                  to={`/project/${proj._id}`}
+                  className="border border-dark rounded pill h-5 w-25"
+                >
+                  Bugs
+                  <span> ({proj.bugs.length})</span>
+                </MDBLink>
+              </div>
             </div>
           ))}
         </div>
-      </>
+      </div>
     )
   }
   return <div>{displayProjects()}</div>
